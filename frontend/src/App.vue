2db5ecb1f9b3bd8ad0a07f36313aaf271e73c13f
RANIT.REN <RANIT.REN@PNC.LOCAL>
2026-02-23 12:01:14 +0700

    home page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './components/home.vue'
+import TheWelcome from './components/package.vue'
 </script>
 
 <template>
