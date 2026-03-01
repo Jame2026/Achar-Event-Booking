82d63c8f5bdcd117e9b8737f137ad2ab4d74a598
linsreymao635-hub <linsreymao635@gmail.com>
2026-03-01 09:28:31 +0700

    push code together

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div class="app-container">
     <router-view />
   </div>
@@ -15,7 +14,3 @@
   z-index: 1;
 }
 </style>
-=======
-  <router-view />
-</template>
->>>>>>> c18a6f1c9c8ca3be1ff8f949e4294140cae53b0b
