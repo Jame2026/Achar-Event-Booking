861ddad79029b7e672c259781a9e2231e0c8dcf9
reamkhorn12345 <reamkhorn12345@gmail.com>
2026-03-02 10:31:18 +0700

    udate contact page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
 import Home from './components/Home.vue'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import Contact from './components/Contact.vue';
 </script>
 
 <template>
   <Home />
+  <Contact />
 </template>
